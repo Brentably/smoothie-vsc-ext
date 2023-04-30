5342cb9bbf6316e2915b01544de7380bd719f3b7
Brentably <Brent@pabnet.com>
2023-04-30 17:07:09 -0600

    debug msg at beginning

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ import {Server} from 'socket.io';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	vscode.window.showInformationMessage(`Did somebody say "smoothies??"`);
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
